8e5eaeabf84bd1f89426a552b94273cd9a5c5800
MwandikiAntony <antonymwandiki26@gmail.com>
2026-02-14 17:15:28 +0300

    added mobile toggle

--- script.js
+++ script.js
@@ -160,3 +160,9 @@ window.addEventListener("scroll", () => {
 });
 
 
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
